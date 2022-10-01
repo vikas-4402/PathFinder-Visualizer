cc2c6b938e883e367e27355aabdf1e25c89f15ab
bhargav2502 <58819249+bhargav2502@users.noreply.github.com>
2022-10-01 20:00:43 +0530

    Added Dijkstra Algorithm into the Visualizer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Visualizer from './Graph/Visualizer/Visualizer.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Visualizer></Visualizer>
+    </div>
+  );
+}
+
+export default App;
